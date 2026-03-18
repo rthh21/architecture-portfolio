4d66bbf5c596ae68c96f2b623e3f1c8308c37ae8
Andrew <andreythstan25@gmail.com>
2026-03-18 19:36:54 +0200

    Made button for go up

--- src/app/components/home/hero/hero.ts
+++ src/app/components/home/hero/hero.ts
@@ -11,6 +11,6 @@ export class Hero {
   isScrollingDown = false;
   @HostListener ('window:scroll', []) onWindowScroll() {
     const currentScroll = window.scrollY;
-    this.isScrollingDown = currentScroll > 900;
+    this.isScrollingDown = currentScroll > 800;
   }
 }
